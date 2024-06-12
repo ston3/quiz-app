d580b3dc8adcbd0dec7f67080417d90b652a6a37
Rainer Flores <rainer.flores64@gmail.com>
2024-06-12 18:02:22 -0400

    added ResultTable component

--- client/src/components/ResultTable.jsx
+++ client/src/components/ResultTable.jsx
@@ -1,6 +1,25 @@
 
 export const ResultTable = () => {
   return (
-    <div>ResultTable</div>
+    <div>
+      <table>
+        <thead className="table-header">
+          <tr className="table-row">
+            <td>Name</td>
+            <td>Attempts</td>
+            <td>Earn Point</td>
+            <td>Result</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="table-body">
+            <td>Daily Tuition</td>
+            <td>02</td>
+            <td>20</td>
+            <td>Passed</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
