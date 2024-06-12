ac0f4837c0fabafb8b15cb41fd045fb6d6049cbc
Rainer Flores <rainer.flores64@gmail.com>
2024-06-12 17:53:53 -0400

    added Result component

--- client/src/components/Result.jsx
+++ client/src/components/Result.jsx
@@ -1,6 +1,55 @@
+import { Link } from 'react-router-dom'
+import '../styles/Result.css'
+import { ResultTable } from './ResultTable'
 
 export const Result = () => {
+  const onRestart = () => {
+
+  }
+
   return (
-    <div>Result</div>
+    <div className="container">
+      <h1 className="title text-light">Quiz App</h1>
+      <div className="result flex-center">
+        <div className="flex">
+          <span>Username</span>
+          <span className="bold">Daily Tuition</span>
+        </div>
+        <div className="flex">
+          <span>Total Quiz Points:</span>
+          <span className="bold">50</span>
+        </div>
+        <div className="flex">
+          <span>Total Questions:</span>
+          <span className="bold">05</span>
+        </div>
+        <div className="flex">
+          <span>Total Attempts</span>
+          <span className="bold">03</span>
+        </div>
+        <div className="flex">
+        <span>Total Earn Points</span>
+        <span className="bold">30</span>
+        </div>
+        <div className="flex">   
+          <span>Quiz Result</span>
+          <span className="bold">Passed</span>
+        </div>
+      </div>
+
+      <div className="start">
+        <Link 
+          className='btn' 
+          to={'/'}
+          onClick={onRestart}
+        >
+          Restart
+        </Link>
+      </div>
+
+      <div className="container">
+        <ResultTable />
+      </div>
+    </div>
   )
 }
---
+++ client/src/components/ResultTable.jsx
@@ -0,0 +1,6 @@
+
+export const ResultTable = () => {
+  return (
+    <div>ResultTable</div>
+  )
+}
